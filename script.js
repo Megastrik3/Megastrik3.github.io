837a9595fb9b4ab617b8c10c5e2cfa1cd71505a9
ryandosch81 <ryandosch81@gmail.com>
2024-10-23 18:49:22 -0400

    MY FIRST COMMIT

---
+++ script.js
@@ -0,0 +1,8 @@
+document.getElementById("otherBtn").addEventListener("click", function() {
+    const inputBox = document.getElementById("locationDD");
+    if (inputBox.style.display === "none") {
+        inputBox.style.display = "block";
+    } else {
+        inputBox.style.display = "none";
+    }
+});
