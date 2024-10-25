7b96c44020aaa2207e476f6d7439bd27f86af270
Megastrik3 <53279958+Megastrik3@users.noreply.github.com>
2024-10-24 20:41:59 -0400

    Moon phase is currently broken. There seems to be an issue with the allowed origins header. Currently working with API dev.

--- js/app.js
+++ js/app.js
@@ -12,6 +12,7 @@ const firebaseConfig = {
     appId: "1:163656577596:web:6fb4a5a1e1c666ac28c939",
     measurementId: "G-9DTNJQNTKN"
 };
+const moonPhaseAuthString = btoa(`84323f10-96d1-4383-92d6-4a0603c0ff03:832f9f8d461c99c999d0a3f9a6f91d7e3a7271cc2a0aea7547351ba64de5d5a7606f2ed9c3bc04d7d27ff151695a8a94841ff233fee9733b588fd93c527187baefbd14d2221d6f659a238399a57fd766e24d1885c337f5fc0ea856b8d9f53a974e1b0db77088c79127a0a91050746736`);
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 //prevents the api kets from being used from an unauthorized source
@@ -93,7 +94,7 @@ async function sendVertexPrompt() {
     // }
     // text = text.replace(/(\r\n|\n|\r)/gm, "");
     // secondaryText = secondaryText.replace(/(\r\n|\n|\r)/gm, "");
-    localStorage.setItem("vertexAI", timestamp + "|" + text + "|" + secondaryText);
+    localStorage.setItem("vertexAI", timestamp + "|" + text);
 
     //prints the output to the console
     console.log("New gen: " + text);
@@ -123,6 +124,7 @@ function checkVertexAge() {
 
     if (getVertexDate != timestamp) {
         const vertexOldDate = getVertexDate.split("-");
+        console.log(vertexOldDate[3] +1 <= dateTime.getHours());
         if (vertexOldDate[3] + 1 <= dateTime.getHours() && vertexOldDate[4] <= dateTime.getMinutes()) {
             console.log("Data more than one hour old -- minute check");
             return true;
@@ -134,4 +136,25 @@ function checkVertexAge() {
     console.log("Data is recent");
     return false;
 }
+
+function getMoonPhaseImage() {
+    let date = new Date();
+    let moonDate = date.getFullYear() + "-" + date.getMonth() + "-" + date.getDate();
+    const data = `{\"style\":{\"moonStyle\":\"default\",\"backgroundStyle\":\"solid\",\"backgroundColor\":\"#ffffff\",\"headingColor\":\"#000000\",\"textColor\":\"#000000\"},\"observer\":{\"latitude\":43.778467,\"longitude\":-74.39033,\"date\":\"${moonDate}\"},\"view\":{\"type\":\"portrait-simple\",\"parameters\":{}}}`;
+
+    const xhr = new XMLHttpRequest();
+    xhr.withCredentials = true;
+    
+    xhr.addEventListener("readystatechange", function () {
+      if (this.readyState === this.DONE) {
+        console.log(this.responseText);
+      }
+    });
+    
+    xhr.open("POST", "https://api.astronomyapi.com/api/v2/studio/moon-phase");
+    xhr.setRequestHeader("Authorization", "Basic " + btoa(`809b1cfe-8b23-410b-b437-4774f5831104:832f9f8d461c99c999d0a3f9a6f91d7e3a7271cc2a0aea7547351ba64de5d5a7606f2ed9c3bc04d7d27ff151695a8a94841ff233fee9733b588fd93c527187ba779f5655b0efc2951aa7b56f73bf247f9b817f622fafc0398bf41f8b06d114abeaa160def0678e7efb4ac3cbba238cde`));
+
+    xhr.send();
+}
 localStorageDataChecks();
+getMoonPhaseImage();
