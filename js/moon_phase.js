7a92b55c56d9bbe2623143a8d36ba0ef368c6c87
Megastrik3 <53279958+Megastrik3@users.noreply.github.com>
2024-11-01 20:44:13 -0400

    Refactor moon phase data handling and display logic; update image IDs for consistency

--- js/moon_phase.js
+++ js/moon_phase.js
@@ -1,9 +1,13 @@
 async function localStorageDataChecks() {
     if (!checkMoonLocalStorage() || checkDataAge()) {
-        getMoonPhase(() => document.getElementById("current-moon-phase").src = localStorage.getItem("moonPhase").split("|")[1]);
+        await getMoonPhase(() => {
+            document.getElementById("current-moon-phase").src = localStorage.getItem("moonPhase").split("|")[1];
+            displayMoonPhases();
+        });
         return;
     }
     document.getElementById("current-moon-phase").src = localStorage.getItem("moonPhase").split("|")[1];
+    displayMoonPhases();
 }
 function checkMoonLocalStorage() {
     if (localStorage.getItem("moonPhase") == null) {
@@ -31,8 +35,8 @@ function checkDataAge() {
     const getData = localStorage.getItem("moonPhase").split("|")[0];
 
     if (getData != getCurrentDate(true)) {
-        const vertexOldDate = getData.split("-");
-     if (parseInt(vertexOldDate[2], 10) != dateTime.getDate() || parseInt(vertexOldDate[1], 10) != parseInt(dateTime.getMonth() + 1) || parseInt(vertexOldDate[0], 10) != dateTime.getFullYear()) {
+        const moonOldDate = getData.split("-");
+     if (parseInt(moonOldDate[2], 10) != dateTime.getDate() || parseInt(moonOldDate[1], 10) != parseInt(dateTime.getMonth() + 1) || parseInt(moonOldDate[0], 10) != dateTime.getFullYear()) {
             console.log("Moon data more than one day old - date check");
             return true;
         }
@@ -41,47 +45,55 @@ function checkDataAge() {
     return false;
 }
 
-function getMoonPhase(_callback) {
+async function getMoonPhase(_callback) {
     const coordinates = localStorage.getItem("currentLocation").split(",");
     const data = `{\"style\":{\"moonStyle\":\"sketch\",\"backgroundStyle\":\"solid\",\"backgroundColor\":\"#ffffff\",\"headingColor\":\"#ffffff\",\"textColor\":\"#000000\"},\"observer\":{\"latitude\":${coordinates[0]},\"longitude\":${coordinates[1]},\"date\":\"2024-10-30\"},\"view\":{\"type\":\"landscape-simple\",\"parameters\":{}}}`;
+    const moonCalendarURL = `https://aa.usno.navy.mil/api/moon/phases/date?date=${getCurrentDate(false)}&nump=5`;
     const xhr = new XMLHttpRequest();
-    
-    xhr.addEventListener("readystatechange", function () {
-      if (this.readyState === this.DONE) {
-        console.log(this.responseText);
-        localStorage.setItem("moonPhase", getCurrentDate(false) + "|" + this.responseText.substring(21, this.responseText.indexOf(".png") + 4));
-        _callback();
-      }
-    });
-    
-    xhr.open("POST", "https://api.astronomyapi.com/api/v2/studio/moon-phase");
-    xhr.setRequestHeader("Authorization", "Basic OTQ4NmY2ZjgtNTIxMC00MWQ4LWI4OWMtMjBlNDM0MzUzNjcxOjgzMmY5ZjhkNDYxYzk5Yzk5OWQwYTNmOWE2ZjkxZDdlM2E3MjcxY2MyYTBhZWE3NTQ3MzUxYmE2NGRlNWQ1YTc2MDZmMmVkOWMzYmMwNGQ3ZDI3ZmYxNTE2OTVhOGE5NDg0MWZmMjMzZmVlOTczM2I1ODhmZDkzYzUyNzE4N2JhNzc5ZjU2NTViMGVmYzI5NTFhYTdiNTZmNzNiZjI0N2Y5YjgxN2Y2MjJmYWZjMDM5OGJmNDFmOGIwNmQxMTRhYmVhYTE2MGRlZjA2NzhlN2VmYjRhYzNjYmJhMjM4Y2Rl");
-    
-    xhr.send(data);
-}
-
-async function getMoonPhaseCalendar(){
-    const url = `https://aa.usno.navy.mil/api/moon/phases/date?date=${getCurrentDate(false)}&nump=5`;
 
     try {
-        const response = await fetch(url);
-        const data = await response.json();
-        console.log(data);
-        document.getElementById("moon-phase-text-1").innerHTML = data.phasedata[0].phase + " - " + new Date(data.phasedata[0].year, data.phasedata[0].month -1).toLocaleString('default', { month: 'long' }) + " " + data.phasedata[0].day;
-        document.getElementById("moon-phase-text-2").innerHTML = data.phasedata[1].phase + " - " + new Date(data.phasedata[1].year, data.phasedata[1].month -1).toLocaleString('default', { month: 'long' }) + " " + data.phasedata[1].day;
-        document.getElementById("moon-phase-text-3").innerHTML = data.phasedata[2].phase + " - " + new Date(data.phasedata[2].year, data.phasedata[2].month -1).toLocaleString('default', { month: 'long' }) + " " + data.phasedata[2].day;
-        document.getElementById("moon-phase-text-4").innerHTML = data.phasedata[3].phase + " - " + new Date(data.phasedata[3].year, data.phasedata[3].month -1).toLocaleString('default', { month: 'long' }) + " " + data.phasedata[3].day;
-        document.getElementById("moon-phase-text-5").innerHTML = data.phasedata[4].phase + " - " + new Date(data.phasedata[4].year, data.phasedata[4].month -1).toLocaleString('default', { month: 'long' }) + " " + data.phasedata[4].day;
-        document.getElementById("first-moon-phase-image").src = "https://upload.wikimedia.org/wikipedia/commons/thumb/e/e1/FullMoon2010.jpg/1200px-FullMoon2010.jpg";
-        document.getElementById("second-moon-phase-image").src = "https://upload.wikimedia.org/wikipedia/commons/thumb/e/e1/FullMoon2010.jpg/1200px-FullMoon2010.jpg";
-        document.getElementById("third-moon-phase-image").src = "https://upload.wikimedia.org/wikipedia/commons/thumb/e/e1/FullMoon2010.jpg/1200px-FullMoon2010.jpg";
-        document.getElementById("fourth-moon-phase-image").src = "https://upload.wikimedia.org/wikipedia/commons/thumb/e/e1/FullMoon2010.jpg/1200px-FullMoon2010.jpg";
-        document.getElementById("fifth-moon-phase-image").src = "https://upload.wikimedia.org/wikipedia/commons/thumb/e/e1/FullMoon2010.jpg/1200px-FullMoon2010.jpg";
-
+        xhr.addEventListener("readystatechange", function () {
+            if (this.readyState === this.DONE) {
+              console.log(this.responseText);
+              localStorage.setItem("moonPhase", getCurrentDate(false) + "|" + this.responseText.substring(21, this.responseText.indexOf(".png") + 4));
+              _callback();
+            }
+          });
+          const moonCalendarResponse = await fetch(moonCalendarURL);
+          const moonPhasesCalendarData = await moonCalendarResponse.json();
+          localStorage.setItem("monthMoonPhase", JSON.stringify(moonPhasesCalendarData));
+          xhr.open("POST", "https://api.astronomyapi.com/api/v2/studio/moon-phase");
+          xhr.setRequestHeader("Authorization", "Basic OTQ4NmY2ZjgtNTIxMC00MWQ4LWI4OWMtMjBlNDM0MzUzNjcxOjgzMmY5ZjhkNDYxYzk5Yzk5OWQwYTNmOWE2ZjkxZDdlM2E3MjcxY2MyYTBhZWE3NTQ3MzUxYmE2NGRlNWQ1YTc2MDZmMmVkOWMzYmMwNGQ3ZDI3ZmYxNTE2OTVhOGE5NDg0MWZmMjMzZmVlOTczM2I1ODhmZDkzYzUyNzE4N2JhNzc5ZjU2NTViMGVmYzI5NTFhYTdiNTZmNzNiZjI0N2Y5YjgxN2Y2MjJmYWZjMDM5OGJmNDFmOGIwNmQxMTRhYmVhYTE2MGRlZjA2NzhlN2VmYjRhYzNjYmJhMjM4Y2Rl");
+          xhr.send(data);
     } catch (error) {
         console.error('Error fetching moon phases:', error);
+    }
+
+}
+
+function displayMoonPhases(){
+    const data = JSON.parse(localStorage.getItem("monthMoonPhase"));
+    document.getElementById("moon-phase-text-1").innerHTML = data.phasedata[0].phase + " - " + new Date(data.phasedata[0].year, data.phasedata[0].month -1).toLocaleString('default', { month: 'long' }) + " " + data.phasedata[0].day;
+    document.getElementById("moon-phase-text-2").innerHTML = data.phasedata[1].phase + " - " + new Date(data.phasedata[1].year, data.phasedata[1].month -1).toLocaleString('default', { month: 'long' }) + " " + data.phasedata[1].day;
+    document.getElementById("moon-phase-text-3").innerHTML = data.phasedata[2].phase + " - " + new Date(data.phasedata[2].year, data.phasedata[2].month -1).toLocaleString('default', { month: 'long' }) + " " + data.phasedata[2].day;
+    document.getElementById("moon-phase-text-4").innerHTML = data.phasedata[3].phase + " - " + new Date(data.phasedata[3].year, data.phasedata[3].month -1).toLocaleString('default', { month: 'long' }) + " " + data.phasedata[3].day;
+    document.getElementById("moon-phase-text-5").innerHTML = data.phasedata[4].phase + " - " + new Date(data.phasedata[4].year, data.phasedata[4].month -1).toLocaleString('default', { month: 'long' }) + " " + data.phasedata[4].day;
+    let elementName = "-moon-phase-image";
+    for (let i = 0; i < 5; i++){
+        if (data.phasedata[i].phase == "New Moon"){
+            document.getElementById(`${i + 1}-moon-phase-image`).src = "https://upload.wikimedia.org/wikipedia/commons/thumb/7/70/Fase_1.jpg/598px-Fase_1.jpg?20161001004830";
+            document.getElementById(`${i + 1}-moon-phase-image`).title = "Source Henrykus on Wikimedia Commons: https://upload.wikimedia.org/wikipedia/commons/thumb/7/70/Fase_1.jpg/598px-Fase_1.jpg?20161001004830";
+        } else if (data.phasedata[i].phase == "First Quarter"){
+            document.getElementById(`${i + 1}-moon-phase-image`).src = "https://upload.wikimedia.org/wikipedia/commons/thumb/e/e1/Fase_13.jpg/598px-Fase_13.jpg?20161001005404";
+            document.getElementById(`${i + 1}-moon-phase-image`).title = "Source Henrykus on Wikimedia Commons: https://upload.wikimedia.org/wikipedia/commons/thumb/e/e1/Fase_13.jpg/598px-Fase_13.jpg?20161001005404";
+        } else if (data.phasedata[i].phase == "Full Moon"){
+            document.getElementById(`${i + 1}-moon-phase-image`).src = "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6b/Fase_9.jpg/598px-Fase_9.jpg?20161001005227";
+            document.getElementById(`${i + 1}-moon-phase-image`).title = "Source Henrykus on Wikimedia Commons: https://upload.wikimedia.org/wikipedia/commons/thumb/6/6b/Fase_9.jpg/598px-Fase_9.jpg?20161001005227";
+        } else if (data.phasedata[i].phase == "Last Quarter"){
+            document.getElementById(`${i + 1}-moon-phase-image`).src = "https://upload.wikimedia.org/wikipedia/commons/thumb/0/04/Fase_6.jpg/598px-Fase_6.jpg?20161001005118";
+            document.getElementById(`${i + 1}-moon-phase-image`).title = "Source Henrykus on Wikimedia Commons: https://upload.wikimedia.org/wikipedia/commons/thumb/0/04/Fase_6.jpg/598px-Fase_6.jpg?20161001005118";
+        }
     }
 }
 
 localStorageDataChecks();
-getMoonPhaseCalendar();
