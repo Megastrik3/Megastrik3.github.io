b75d35b16a566267c2bc1c4562fe942b4b34ab1d
ryandosch81 <ryandosch81@gmail.com>
2024-11-11 12:54:52 -0500

    Fixed Other button

--- script.js
+++
@@ -1,9 +0,0 @@
-document.getElementById("otherBtn").addEventListener("click", function() {
-    const inputBox = document.getElementById("locationDD");
-    if (inputBox.style.display === "none") {
-        inputBox.style.display = "block";
-    } else {
-        inputBox.style.display = "none";
-    }
-}); 
-
