6673b7566e3271e372ac56a38c7bacdd1bcafcfb
Megastrik3 <53279958+Megastrik3@users.noreply.github.com>
2024-11-19 21:20:16 -0500

    added moon fase images to local files

--- js/moon_phase.js
+++ js/moon_phase.js
@@ -72,16 +72,16 @@ function displayMoonPhaseData() {
     for (let i = 0; i < 5; i++) {
         document.getElementById(`moon-phase-text-${i + 1}`).innerHTML = data.phasedata[i].phase + " - " + new Date(data.phasedata[i].year, data.phasedata[i].month - 1, data.phasedata[i].day).toLocaleString('en-US', { month: 'long', day: 'numeric' });
         if (data.phasedata[i].phase == "New Moon") {
-            document.getElementById(`${i + 1}-moon-phase-image`).src = "https://upload.wikimedia.org/wikipedia/commons/thumb/7/70/Fase_1.jpg/598px-Fase_1.jpg?20161001004830";
+            document.getElementById(`${i + 1}-moon-phase-image`).src = "../assests/Fase_1.jpg";
             document.getElementById(`${i + 1}-moon-phase-image`).title = "Source Henrykus on Wikimedia Commons: https://upload.wikimedia.org/wikipedia/commons/thumb/7/70/Fase_1.jpg/598px-Fase_1.jpg?20161001004830";
         } else if (data.phasedata[i].phase == "First Quarter") {
-            document.getElementById(`${i + 1}-moon-phase-image`).src = "https://upload.wikimedia.org/wikipedia/commons/thumb/e/e1/Fase_13.jpg/598px-Fase_13.jpg?20161001005404";
+            document.getElementById(`${i + 1}-moon-phase-image`).src = "../assests/Fase_13.jpg";
             document.getElementById(`${i + 1}-moon-phase-image`).title = "Source Henrykus on Wikimedia Commons: https://upload.wikimedia.org/wikipedia/commons/thumb/e/e1/Fase_13.jpg/598px-Fase_13.jpg?20161001005404";
         } else if (data.phasedata[i].phase == "Full Moon") {
-            document.getElementById(`${i + 1}-moon-phase-image`).src = "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6b/Fase_9.jpg/598px-Fase_9.jpg?20161001005227";
+            document.getElementById(`${i + 1}-moon-phase-image`).src = "../assests/Fase_9.jpg";
             document.getElementById(`${i + 1}-moon-phase-image`).title = "Source Henrykus on Wikimedia Commons: https://upload.wikimedia.org/wikipedia/commons/thumb/6/6b/Fase_9.jpg/598px-Fase_9.jpg?20161001005227";
         } else if (data.phasedata[i].phase == "Last Quarter") {
-            document.getElementById(`${i + 1}-moon-phase-image`).src = "https://upload.wikimedia.org/wikipedia/commons/thumb/0/04/Fase_6.jpg/598px-Fase_6.jpg?20161001005118";
+            document.getElementById(`${i + 1}-moon-phase-image`).src = "../assests/Fase_6.jpg";
             document.getElementById(`${i + 1}-moon-phase-image`).title = "Source Henrykus on Wikimedia Commons: https://upload.wikimedia.org/wikipedia/commons/thumb/0/04/Fase_6.jpg/598px-Fase_6.jpg?20161001005118";
         }
     }
