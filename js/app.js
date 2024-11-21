83e55de25aaa1085390e27f91161259e45fc29b1
Megastrik3 <53279958+Megastrik3@users.noreply.github.com>
2024-11-20 20:01:53 -0500

    VertexAI Time Check Fix (#97)
    * added moon fase images to local files
    
    * Fixed hourly time check to work at 11pm.

--- js/app.js
+++ js/app.js
@@ -7,14 +7,18 @@ export function checkLocalStorage(storageKey) {
 }
 export function checkAge(frequency, localStorageKey) {
     const dateTime = new Date();
+    let i = 0;
     const getData = localStorage.getItem(localStorageKey).split("|")[0];
     if (getData == getCurrentDate(true) || getData == getCurrentDate(false)) {
         console.log(`Data is less than one hour old ${localStorageKey}`);
         return false;
     }
     const localStorageTimestamp = getData.split("-");
+    if (dateTime.getHours() == 0) {
+        i = 24;
+    }
     if (frequency == "hourly") {
-            if (parseInt(localStorageTimestamp[3], 10) + 1 <= dateTime.getHours() || parseInt(localStorageTimestamp[4], 10) >= dateTime.getMinutes() && parseInt(localStorageTimestamp[3], 10) + 1 == dateTime.getHours()) {
+            if (parseInt(localStorageTimestamp[3], 10) + 1 < dateTime.getHours() + i || parseInt(localStorageTimestamp[4], 10) >= dateTime.getMinutes() && parseInt(localStorageTimestamp[3], 10) + 1 == dateTime.getHours() + i) {
                 console.log(`Data more than one hour old -- time check ${localStorageKey}`);
                 return true;
             } else if (parseInt(localStorageTimestamp[2], 10) != dateTime.getDate() || parseInt(localStorageTimestamp[1], 10) != parseInt(dateTime.getMonth() + 1) || parseInt(localStorageTimestamp[0], 10) != dateTime.getFullYear()) {
